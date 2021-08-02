0726e9d5d15aaf95a96ec4450cdf9fa25e8b03bc
Mijojac <mijojac@gmail.com>
2021-08-02 08:01:08 +0200

    Initial Commit

---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Miroslav Mali on 2.8.21..
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
